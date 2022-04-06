0a16e5dacb8d9332edef4ce1910ae485606a6f2d
Lorelai Knutti <virgoknight2@gmail.com>
2022-04-06 11:51:49 -0400

    I got the second half to run I guess

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(object){
+    for (const record of object) {
+        if (record.result == "w") {
+          return console.log(record.year(object));
+        }
+ 
+    }   
+}
